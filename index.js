5d20da02aa583de4f623612b19e041445ce1fbfb
Drazen Drinic <drazendrinic@hotmail.com>
2025-05-10 21:42:18 +0200

    Set: firebase app

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.7.1/firebase-app.js';
+import {
+  getDatabase,
+  ref,
+  push,
+} from 'https://www.gstatic.com/firebasejs/11.7.1/firebase-database.js';
+
+const firebaseConfig = {
+  databaseURL:
+    'https://cart-b1483-default-rtdb.europe-west1.firebasedatabase.app/',
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+const shoppingListDB = ref(database, 'drinicCart');
+
 const inputFieldEl = document.getElementById('input-field');
 const buttonAddEl = document.getElementById('button-add');
 
 buttonAddEl.addEventListener('click', function () {
   if (inputFieldEl.value) {
     const inputFieldValue = inputFieldEl.value;
+
+    push(shoppingListDB, inputFieldValue);
+
     console.log(inputFieldValue);
+
+    inputFieldEl.value = '';
   }
 });
